ac5577e236aa6cbab9fee4b0a82ea29b4b3e4677
luigipr <luigiptofeti@gmail.com>
2024-07-23 18:16:52 -0300

    snake game w/o build

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ screen = Screen()
 
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("snake game")
+screen.title("Snake game")
 screen.tracer(0)
 game = True
 
@@ -49,3 +49,4 @@ while game:
 
 
 screen.exitonclick()
+
